fb53a3fd92de695044e36553147c362642f4073c
barziv5 <barziv4@gmail.com>
2014-12-25 21:01:38 +0200

    Added temporary threads counter

--- src/WebServer.java
+++ src/WebServer.java
@@ -4,8 +4,12 @@ import java.util.* ;
 
 public final class WebServer
 {
+	private static int numberOfThreads = 0;
 	
 	public WebServer(int port,int maxThreads){ //Add threads counter
+		//TEMPORARY
+		this.numberOfThreads = 0;
+
 		
 		// Establish the listen socket.
 		ServerSocket socket = null;
@@ -14,16 +18,19 @@ public final class WebServer
 		} catch (IOException e2) {
             System.out.println("Could not create server socket on port: " + port);
 		}
+		System.out.println("listening to: " + port);
 		
 		// Process HTTP service requests in an infinite loop.
 		while (true)
 		{
+
 		    // Listen for a TCP connection request.
 		    Socket connection = null;
 			try {
 				connection = socket.accept();
-			} catch (IOException e1) {
+			} catch (Exception ex) {
 				System.out.println("Encoutered error on accapting connection");
+				System.exit(1);
 			}
 		    
 		    // Construct an object to process the HTTP request message.
@@ -33,18 +40,31 @@ public final class WebServer
 			} catch (Exception e) {
 				System.out.println("Encoutered error on creating new Http request");
 			}
-		    
-		    // Create a new thread to process the request.
-		    Thread thread = new Thread(request);
-		    
-		    // Start the thread.
-		    thread.start();
+			
+			if (numberOfThreads == maxThreads){
+				System.out.println("Server is Busy");
+			}else{
+			    // Create a new thread to process the request.
+			    Thread thread = new Thread(request);
+			    
+			    // Start the thread.
+			    thread.start();
+			    this.numberOfThreads++;
+			    System.out.println(this.numberOfThreads);
+			}
+
 		}
 	}
+	
+	//TEMPORARY, to be used by HTTPRESPONSE
+	public static void closedThread(){
+		numberOfThreads--;
+	}
+	
 	public static void main(String argv[]) throws Exception
 	{	
 		//read config file
-		ConfigFileReader currentConfigs = new ConfigFileReader("/config.ini");
+		ConfigFileReader currentConfigs = new ConfigFileReader("C:/config.ini");
 		//get server settings
 		HttpServerSettings currentSetting = currentConfigs.getHttpServerSettings();
 		//creating web server
