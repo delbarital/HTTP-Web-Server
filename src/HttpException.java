cfadf361954664e692a90418b3ec4e05ae8def77
Tal <delbarital@gmail.com>
2014-12-26 02:21:50 +0200

    changing a little bit the format of the exception string

--- src/HttpException.java
+++ src/HttpException.java
@@ -3,9 +3,9 @@ public class HttpException extends Exception {
 
 	public HttpException(int exceptionCode) {
 		// TODO: currently the exception message does not contains the error number. We should fix it 
-		super(new HttpResponseCode(exceptionCode).getResponseMessageString());
+		super(exceptionCode + " ," + new HttpResponseCode(exceptionCode).getResponseMessageString());
 	}
-
+	
 	public HttpException() {}
 
 }
