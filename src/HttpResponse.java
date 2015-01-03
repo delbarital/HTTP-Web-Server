14ce2b7b69770b1ac7183d3a5f55434667861feb
Tal <delbarital@gmail.com>
2015-01-03 16:23:52 +0200

    styling

--- src/HttpResponse.java
+++ src/HttpResponse.java
@@ -5,7 +5,44 @@
  */
 public class HttpResponse {
 
-	HttpStatusCode httpStatusCode;
 	String httpVersion;
+	HttpStatusCode httpStatusCode;	
+
+	public HttpResponse(String httpVersion, HttpStatusCode statusCode)
+			throws HttpException {
+		// set version
+		if (httpVersion.toUpperCase() == "HTTP/1.0"
+				|| httpVersion.toLowerCase() == "HTTP/1.1") {
+			this.httpVersion = httpVersion.toUpperCase();
+		} else {
+			// unknown or unsupported HTTP version
+			throw new HttpException(501);
+		}
+		
+		// set status code
+		if (statusCode == null) {
+			// No status code was received.
+			throw new HttpException(501);
+		}
+		this.httpStatusCode = statusCode;
+	}
+
 	
+	// Getters
+	
+	/**
+	 * returns the fist line of the HTTP response, the status line, as described
+	 * in the RFC: 
+	 * HTTP-Version SP Status-Code SP Reason-Phrase CRLF
+	 */
+	public String getStatusLine() {
+		String crlf = "\r\n";
+		return this.httpVersion + " " + this.httpStatusCode.getStatusCode()
+				+ " " + this.httpStatusCode.getResponseMessageString() + crlf;
+	}
+	
+	public HttpStatusCode getStatusCode() {
+		return this.httpStatusCode;
+	}	
+
 }
